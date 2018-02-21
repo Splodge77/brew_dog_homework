ca28f9b57d7f4d17b6308f0d34862df5cb7e3679
Max Robson <maxwell.robson@gmail.com>
2018-02-21 23:33:19 +0000

    added selector and some css

--- punk_api_start_code/public/app.js
+++ punk_api_start_code/public/app.js
@@ -17,13 +17,35 @@ const requestComplete = function(){
   if(this.status !== 200) return;
   const jsonString = this.responseText;
   const beers = JSON.parse(jsonString);
-  console.log(beers);
   populateList(beers);
+  populateDropdown(beers);
 };
 
-// const handleSelectChanged = function(beers, index){
-//   setPtags(beers, index);
-// }
+const handleSelectChanged = function(beers, index){
+  setPtags(beers, index);
+}
+
+const setPtags = function(beers, index){
+  const pTag1 = document.querySelector('#beer-select-result');
+  pTag1.innerText = beers[index].name;
+  const pTag2 = document.querySelector('#tagline-select-result');
+  pTag2.innerText = beers[index].tagline;
+  const pTag3 = document.querySelector('#description-select-result');
+  pTag3.innerText = beers[index].description;
+};
+
+const populateDropdown = function(beers){
+  const select = document.getElementById("beer-select");
+  beers.forEach(function(beer, index){
+    const option = document.createElement('option');
+    option.innerText = beer.name;
+    option.value = index;
+    select.appendChild(option);
+  });
+  select.addEventListener("change", function(){
+    handleSelectChanged(beers, this.value)
+  });
+};
 
 const populateList = function(beers){
   const dl = document.getElementById("beer-list");
@@ -33,23 +55,10 @@ const populateList = function(beers){
     const image = document.createElement('img')
     image.src = beer.image_url;
     image.width = '50';
-    image.height = '150';
-    // dd.innerText = beer.image_url;
-    console.log(beer);
+    image.height = '170';
     dl.appendChild(dt);
     dl.appendChild(image);
   });
 };
-
-// const createImg = function(url){
-//   const img = document.createElement('img');
-//   img.src = url;
-// }
-
-// const setPtags = function(beers, index){
-//   const pTag1 = document.querySelector('#country-select-result');
-//   pTag1.innerText = "You have selected: " + beers[index].name;
-// };
-
 
 document.addEventListener('DOMContentLoaded', app);
